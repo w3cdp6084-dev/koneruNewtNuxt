dd82f0e4ee37a8f7de04eb7ab266cd69ad10d840
w3cdp6084-dev <yusukew3@gmail.com>
2023-05-14 09:24:49 +0900

    Nav add

--- components/Header.vue
+++ components/Header.vue
@@ -1,19 +1,8 @@
+<script>
+  import Nav from '@/components/Nav.vue';
+</script>
 <template>
   <header>
-    <nav>
-      <ul>
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/about">About</router-link></li>
-        <li><router-link to="/contact">Contact</router-link></li>
-      </ul>
-    </nav>
+    <Nav />
   </header>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'Header'
-})
-</script>
